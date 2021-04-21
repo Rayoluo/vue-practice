499eb8bda4bde3b0b08bde4fdb65c2f8bd658f0a
oluoluo <hustly123@gmail.com>
2021-04-21 21:34:54 +0800

    添加登录功能以及/home的基本结构

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,11 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
 
 Vue.use(Button)
+// 将导入的组件注册为全局可用
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+// Message的使用方式略微不同，需要进行挂载
+// 将Message组件挂载到Vue的原型对象上，这样每个组件都可以通过this.$message使用该组件
+Vue.prototype.$message = Message
